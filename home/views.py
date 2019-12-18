f860a216c686822a876351f6cf440cca5d3ec1b9
phpervip <2998658517@qq.com>
2019-12-18 16:42:43 +0800

    Config views & urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    print('{}: Request Home Page!'.format(request.get_host()))
+    return render(request, 'index.html', {'body_title': 'Home Page'})
--- note/views.py
+++ note/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    print('{}: Request Note Page!'.format(request.get_host()))
+    return render(request, 'note.html', {'head_title': 'Sthtodo Note', 'body_title': 'Note Page'})
